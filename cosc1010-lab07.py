72b35c90665ecf08e27e03f74feb110bc4e44710
C00psta <clilly1@uwyo.edu>
2024-11-04 07:08:26 +0000

    finished

--- cosc1010-lab07.py
+++ cosc1010-lab07.py
@@ -1,12 +1,10 @@
-# Your Name Here
+# Cooper Lilly
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# Submission Date 11/3/24
+# Lab 7
+# Lab Section: 13
+# Sources, people worked with, help given to: Brayden 
+# couldn't turn in on time due to gas leak :(
 
 
 # Prompt the user for an upper bound 
@@ -17,9 +15,25 @@
     # If a user did not enter a number output a statement saying so
 # You will continue to prompt the user until a proper integer value is entered
 
-factorial = 1
+while True:
+    user_upper_bound = input("to calculate the factorial type a positvie number. (or type esc to end the sequence.): ")
 
-print(f"The result of the factorial based on the given bound is {factorial}")
+    if user_upper_bound.upper() == 'esc':
+        break
+
+#now 
+
+    if user_upper_bound.isdigit():
+        user_upper_bound + int(user_upper_bound)
+        factorial = 1
+        for i in range(1, user_upper_bound + 1):
+            factorial = 1
+        print(f"The result of the factorial based on the given bound is {factorial}")
+
+        break
+
+else:
+    print("Thats not a positive number try again, please. :)")
 
 print("*"*75)
 # Create a while loop that prompts a user for input of an integer values
@@ -39,7 +53,27 @@ print("*"*75)
 
 num_sum = 0 
 
-print(f"Your final sum is {num_sum}")
+while True:
+    user_input = input("type a positvie number. (or type esc to end the sequence.): ")
+
+    if user_input.upper() == 'esc':
+        print(f"Your final sum is {num_sum}")
+        break
+
+    if user_input.startswith('-'):
+
+        if user_input[1:].isdigit():
+            num_sum += int(user_input)
+        else:
+            print('not valid. think harder and try again. ')
+
+    elif user_input.isdigit():
+         num_sum += int(user_input)
+
+    else:
+        print('not valid. think harder and try again. ')
+
+
 
 print("*"*75)
 # Now you will be creating a two operand calculator
@@ -60,3 +94,49 @@ print("*"*75)
 # Again, loop through prompting the user for input until `exit` in any casing is input 
 
         
+while True:
+    user_input = input("type a positvie number. (or type esc to end the sequence.): ")
+
+    if user_input.upper() == 'esc':
+        break
+
+    user_input = user_input.replace("","")
+    usable_operators = "+-/*%"
+    operator = None
+
+    for ops in usable_operators:
+        if ops in user_input:
+            operator = ops
+            break
+
+    if operator:
+        operates = user_input.split(operator)
+
+        if len(operates) == 2 and all(op.isdigit() for op in operates):
+            number1 = int(operates[0])
+            number2 = int(operates[1])
+
+            if operator == "+":
+                result_val = number1 + number2
+            
+            elif operator == "-":
+                result_val = number1 - number2
+
+            elif operator == "/":
+                if number2 != 0:
+                    result_val = number1 / number2
+                    
+            elif operator == "*":
+                result_val = number1 * number2
+            
+            elif operator == "%":
+                result_val = number1 % number2
+
+            print(f"calculation eqauls: {result_val}")
+
+        else:
+            print("not valid. enter 2 numbers and a operator")
+
+    else:
+        print("not vaild. input an available operator like:+,-,*,%,/")
+            
